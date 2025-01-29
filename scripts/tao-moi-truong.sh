ab37bf6a2d4a8fa6620ed2d94d8a9b4af4804886
LapTQ <lap.tq4@gmail.com>
2025-01-29 22:58:19 +0700

    msg

--- scripts/tao-moi-truong.sh
+++ scripts/tao-moi-truong.sh
@@ -3,8 +3,7 @@ venv_parent=$( realpath $DIR/.. )
 venv_name=.venv
 venv_path=$venv_parent/$venv_name
 [[ ! -d $venv_path ]] && py -m venv $venv_path
-cd $venv_path
-./Scripts/activate
-which python3
+. $venv_path/Scripts/activate
+py -c "import os, sys; print(os.path.dirname(sys.executable))"
 
 py -m pip install --ignore-installed -r requirements.txt
